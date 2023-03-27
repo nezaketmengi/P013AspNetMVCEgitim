c8d43ccfbbb2708e88272058e77f4f8bea91a81e
nezaketmengi <nezaketmengi.24@gmail.com>
2023-03-27 23:01:07 +0300

    27.03.2023 Session

--- P013AspNetMVCEgitim/Controllers/MVC08ViewResultsController.cs
+++ P013AspNetMVCEgitim/Controllers/MVC08ViewResultsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using P013AspNetMVCEgitim.Models;
 
 namespace P013AspNetMVCEgitim.Controllers
 {
@@ -19,9 +20,48 @@ namespace P013AspNetMVCEgitim.Controllers
             //return RedirectToAction("Index"); // metot çalıştığında aynı controllerdaki bir actiona yönlendirmemizi sağlar
             return RedirectToAction("Index", "Home"); //metot çalıştığında farklı bir controller daki actiona bu şekilde yönlendirebiliriz.
         }
-        public IActionResult RouteYonlendir()
+        public RedirectToRouteResult RouteYonlendir() // IActionResult da yapsaydık olurdu.
         {
             return RedirectToRoute("Default", new { controller="Home", action="Index", id = 18}); // metot çalıştığında route sistemiyle yönlendirme yapmamızı sağlar
+        }
+        public PartialViewResult KategorileriGetirPartial() // IActionResult da yapsaydık olurdu.
+        {
+            return PartialView("_KategorilerPartial");
+        }
+        public IActionResult XmlContentResult()
+        {
+            var xml = @"
+                <kullanicilar>
+                    <Kullanici>
+                        <Adi>
+                            Nezaket
+                        </Adi>
+                        <Soyadi>
+                            Mengi
+                        </Soyadi>
+                    </Kullanici>
+                    <Kullanici>
+                        <Adi>
+                            Tolga
+                        </Adi>
+                        <Soyadi>
+                            Sonat
+                        </Soyadi>
+                    </Kullanici>
+
+                </kullanicilar>
+
+            ";
+            return Content(xml, "application/xml"); // geriye xml içeriğini döndürdük. "application/xml" tipini belirledikten sonra bildiğin xml yapısına geliyor.
+        }
+
+        public IActionResult JsonDondur()
+        {
+            var kullanici = new Kullanici()
+            {
+                Ad = "Nezaket", Soyad = "Mengi", KullaniciAdi = "Nez"
+            };
+            return Json(kullanici);
         }
     }
 }
